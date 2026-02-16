5f23a00a9e7f4d0ed9fa9b4eb3d4a7ac8969cea0
matT-HT295 <mathias.turiga.295@my.htoakville.ca>
2026-02-16 00:08:32 -0500

    dependencies and containers added

---
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonPipelineResult;
+
+public class Vision {
+
+}
